35319df0d59b3b7b87aa25f21537617fdb16eb3e
stant <stantondobson@gmail.com>
2020-04-03 15:41:34 -0400

    Updates

---
+++ precache-manifest.77bba3e29132683fbb574330c6feb43a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3523e7511206ffb2c52bd57e3e916588",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0dff402ab5c3fecd8260",
+    "url": "/monsters-rolodex/static/css/main.ac22887e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dff402ab5c3fecd8260",
+    "url": "/monsters-rolodex/static/js/main.7aa3b58a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
